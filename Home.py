663f231bbe5eb50cadcc86946f1ebeda79afa61b
Nipun-Chaudhari <nipunchaudharinsc@gmail.com>
2023-05-03 14:30:40 +0530

    Faltu commit

--- Home.py
+++ Home.py
@@ -28,7 +28,4 @@ if button:
         pipeline.fit(review_train, label_train)
         pred = pipeline.predict([review])
 
-        st.success(pred[0])
-
-
-
+        st.success('This review is ', pred[0])
